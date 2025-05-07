02df9e03b3db51a063dc8442d661420fd041a408
Eduardo Júnior <edu.antoniojunior115@gmail.com>
2025-05-06 22:02:17 -0300

    feature/class-model - Criação parcial das classes Usuário e Notícia

--- arqweb1/src/main/java/br/edu/ifsp/g2/model/Noticia.java
+++ arqweb1/src/main/java/br/edu/ifsp/g2/model/Noticia.java
@@ -1,5 +1,51 @@
 package br.edu.ifsp.g2.model;
 
-public class Noticia {
+import java.time.LocalDate;
 
+public class Noticia {
+	
+	private String titulo;
+	private String texto;
+	private LocalDate dataPublicacao;
+	private String nomeAutor;
+	
+	//O nome do autor não é necessário por questões de tema da notícia e segurança do jornalista
+	
+	public Noticia(String titulo, String texto, LocalDate dataPublicacao) {
+		setTitulo(titulo);
+		setTexto(texto);
+		setDataPublicacao(dataPublicacao);
+	}
+	
+	public String getTitulo() {
+		return titulo;
+	}
+	
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	
+	public String getTexto() {
+		return texto;
+	}
+	
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	
+	public LocalDate getDataPublicacao() {
+		return dataPublicacao;
+	}
+	
+	public void setDataPublicacao(LocalDate dataPublicacao) {
+		this.dataPublicacao = dataPublicacao;
+	}
+	
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+	
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
 }
--- arqweb1/src/main/java/br/edu/ifsp/g2/model/Usuario.java
+++ arqweb1/src/main/java/br/edu/ifsp/g2/model/Usuario.java
@@ -1,5 +1,48 @@
 package br.edu.ifsp.g2.model;
 
 public class Usuario {
+	
+	private String usuario;
+	private String senha;
+	private String nome;
+	private int idade;
+	
+	public Usuario(String usuario, String senha, String nome, int idade) {
+		setUsuario(usuario);
+		setSenha(senha);
+		setNome(nome);
+		setIdade(idade);
+	}
 
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 }
