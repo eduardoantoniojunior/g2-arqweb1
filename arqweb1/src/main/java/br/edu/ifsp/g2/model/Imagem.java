a8ed82f74ad590a541ae2c6c4a3462ec356d47fa
Eduardo Júnior <edu.antoniojunior115@gmail.com>
2025-05-13 22:05:37 -0300

    Implementação e manipulação de Imagens com o ImageIO

---
+++ arqweb1/src/main/java/br/edu/ifsp/g2/model/Imagem.java
@@ -0,0 +1,14 @@
+package br.edu.ifsp.g2.model;
+
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class Imagem {
+	
+	private static int ultimoId = 0;
+	private ImageIO imagem;
+	private int id;
+	
+	
+}
